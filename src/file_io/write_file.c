d764eed45557a4d28dfe55708034acd0d9d4ff76
Aviad <aviad.bagno@gmail.com>
2021-02-14 10:54:39 +0200

    Now write_file also writes body. write_file is ready.

--- src/file_io/write_file.c
+++ src/file_io/write_file.c
@@ -7,6 +7,15 @@
 
 #define HEADER_MAGIC 0x3910
 
+int write_string(FILE* file, char* string)
+{
+    int length = strlen(string) + 1; // Plus one for null terminator.
+    if (fwrite(string, length, 1, file) != 1)
+        return 0; // ERROR
+
+    return 1;
+}
+
 int write_column(FILE* file, struct s_column column)
 {
     enum e_type type = column.type;
@@ -18,11 +27,7 @@ int write_column(FILE* file, struct s_column column)
 
     // Write name
     if (type != ERROR)
-    {
-        int length = strlen(name) + 1; // Plus one for null terminator.
-        if (fwrite(name, length, 1, file) != 1)
-            return 0; // ERROR
-    }
+        return write_string(file, name);
 
     return 1;
 }
@@ -47,9 +52,8 @@ int write_table_header(FILE* file, struct s_table_header table_header)
     if (fwrite(&magic, 2, 1, file) != 1) 
         return 0; // ERROR
 
-    // Write DB name
-    int length = strlen(table_header.table_name)+1; // Plus one for null terminator
-    if (fwrite(table_header.table_name, length, 1, file) != 1)
+    char* table_name = table_header.table_name;
+    if (write_string(file, table_name) == 0)
         return 0; // ERROR
 
     // Write DB columns
@@ -59,9 +63,39 @@ int write_table_header(FILE* file, struct s_table_header table_header)
     return 1;
 }
 
+int write_field(FILE* file, struct s_data_holder field)
+{
+    if (field.type == STRING) 
+        return write_string(file, field.data.string);
+    else if (field.type == INTEGER)
+        return fwrite(&field.data.integer, sizeof(int), 1, file) != 0;
+    else
+    {
+        char null = 0;
+        return fwrite(&null, sizeof(char), 1, file) != 0;
+    }
+}
+
+int write_row(FILE* file, struct s_row row)
+{
+    for (int i = 0; i < row.length; i++)
+    {
+        if (write_field(file, row.fields[i]) == 0) 
+            return 0; // ERROR
+    }
+    
+    return 1;
+}
+
 int write_table_body(FILE* file, struct s_table_body table_body)
 {
-    return 0;
+    for (int i = 0; i < table_body.length; i++)
+    {
+        if (write_row(file, table_body.rows[i]) == 0) 
+            return 0; // ERROR
+    }
+    
+    return 1;
 }
 
 // Returns zero if there is any error; Else, return non-zero number.
@@ -70,7 +104,10 @@ int write_file(struct s_table table, char* file_name)
     FILE* file = fopen(file_name, "wb");
     if (file == NULL) return 0;
     
-    if (write_table_header(file, table.table_header) == 0 || write_table_body(file, table.table_body))
+    if (write_table_header(file, table.table_header) == 0 
+        || 
+        write_table_body(file, table.table_body) == 0
+    )
         return 0; // ERROR
 
     return 1;
