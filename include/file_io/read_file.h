b9b2787f1ebc5a060ef4df16bf654e0f9b07983c
Aviad <aviad.bagno@gmail.com>
2021-02-14 14:50:19 +0200

    Created a skeleton for read_file

---
+++ include/file_io/read_file.h
@@ -0,0 +1,3 @@
+#include "data_types/table.h"
+
+int read_file(char* file_name, struct s_table* table);
---
+++ src/file_io/read_file.c
@@ -0,0 +1,6 @@
+#include "file_io/write_file.h"
+
+int read_file(char* file_name, struct s_table* table)
+{
+    return 0;
+}
