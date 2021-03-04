2a3231a8bf66f1901a5c18b7af9947e7a1f415a4
Aviad <aviad.bagno@gmail.com>
2021-03-04 20:49:42 +0200

    Reading columns structure

--- src/file_io/read_file.c
+++ src/file_io/read_file.c
@@ -36,9 +36,57 @@ e_status read_string_from_file(FILE* file, char** string_ptr)
 
     // Allocate memory for the string
     *string_ptr = (char* ) malloc(sizeof(char) * (string_length + 1));
-    if (fread(*string_ptr, sizeof(char), string_length, file) < string_length) 
+    if (fread(*string_ptr, sizeof(char), string_length+1, file) < string_length) 
         return ERROR;
-    *(*string_ptr + string_length) = 0; // Null terminator
+
+    return SUCCESS;
+}
+
+e_status read_int_from_file(FILE* file, int* n)
+{
+    if (fread(n, sizeof(int), 1, file) < 1) 
+        return ERROR;
+
+    return SUCCESS;
+}
+
+// column->name should be explicity freed.
+e_status read_column(FILE* file, struct s_column* column)
+{
+    int type;
+    if (read_int_from_file(file, &type) == ERROR)
+        return ERROR;
+    column->type = type;
+
+    char* name;
+    if (read_string_from_file(file, &name) == ERROR)
+        return ERROR;
+    column->name = name;
+
+    return SUCCESS;
+}
+
+// columns_structure->columns should be explicity freed.
+e_status read_columns_structure(FILE* file, struct s_columns_structure* columns_structure)
+{
+    int length;
+    if (read_int_from_file(file, &length) == ERROR)
+        return ERROR;
+    columns_structure->length = length;
+
+    struct s_column* columns = malloc(sizeof(struct s_column) * length);
+    if (columns == NULL) return ERROR;
+
+    for (int i = 0; i < length; i++)
+    {
+        struct s_column column;
+        if (read_column(file, &column) == ERROR)
+            return ERROR;
+
+        columns[i] = column;
+    }
+
+    columns_structure->columns = columns;
 
     return SUCCESS;
 }
@@ -50,12 +98,17 @@ e_status read_header(FILE* file, struct s_table_header* header)
     if (read_string_from_file(file, &table_name) == ERROR) return ERROR;
     header->table_name = table_name;
 
+    struct s_columns_structure columns_structure;
+    if (read_columns_structure(file, &columns_structure) == ERROR)
+        return ERROR;
+    header->columns_structure = columns_structure;
+
     return SUCCESS;
 }
 
 
 // *body should be initialized.
-e_status read_body(FILE* file, struct s_table_body* body)
+e_status read_body(FILE* file, struct s_columns_structure columns_structure, struct s_table_body* body)
 {
     return SUCCESS;
 }
@@ -91,7 +144,7 @@ e_status read_file(char* file_name, struct s_table* table)
     if (read_header(file, &header) == ERROR) goto close_and_error;;
     
     struct s_table_body body;
-    if (read_body(file, &body) == ERROR) goto close_and_error;;
+    if (read_body(file, header.columns_structure, &body) == ERROR) goto close_and_error;;
 
     table->table_header = header;
     table->table_body = body;
